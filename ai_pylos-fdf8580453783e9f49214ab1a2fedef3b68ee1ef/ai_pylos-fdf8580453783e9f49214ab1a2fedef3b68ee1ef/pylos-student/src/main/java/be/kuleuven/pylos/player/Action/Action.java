5ad3a68cf75694005021bcdbfc4fbf7c34aba0c5
Victor <57635691+mortkever@users.noreply.github.com>
2024-10-10 10:02:23 +0200

    simulate do + undo

--- ai_pylos-fdf8580453783e9f49214ab1a2fedef3b68ee1ef/ai_pylos-fdf8580453783e9f49214ab1a2fedef3b68ee1ef/pylos-student/src/main/java/be/kuleuven/pylos/player/Action/Action.java
+++ ai_pylos-fdf8580453783e9f49214ab1a2fedef3b68ee1ef/ai_pylos-fdf8580453783e9f49214ab1a2fedef3b68ee1ef/pylos-student/src/main/java/be/kuleuven/pylos/player/Action/Action.java
@@ -1,67 +1,74 @@
 package be.kuleuven.pylos.player.Action;
+
 import be.kuleuven.pylos.game.PylosGameIF;
 import be.kuleuven.pylos.game.PylosLocation;
 import be.kuleuven.pylos.game.PylosSphere;
 import be.kuleuven.pylos.game.PylosGameSimulator;
+import be.kuleuven.pylos.game.PylosGameState;
 
 public class Action {
-    // later: lijst van alle mogelijke toekomstige moves (eigen moves en moves tegenstander, dus ook kleur meegeven)
-    // en dan daaruit later beste kan kiezen
-    // of state (er was nog 1)
-    
-    ActionType TYPE;
-    PylosSphere SPHERE;
-    PylosLocation TO;
-    PylosLocation FROM;
+  // later: lijst van alle mogelijke toekomstige moves (eigen moves en moves
+  // tegenstander, dus ook kleur meegeven)
+  // en dan daaruit later beste kan kiezen
+  // of state (er was nog 1)
 
-    // ...
-    
+  ActionType TYPE;
+  PylosSphere SPHERE;
+  PylosLocation TO;
+  PylosLocation FROM;
+  PylosGameState prevState;
 
-    public Action(PylosSphere sphere, PylosLocation to, PylosLocation from, ActionType type) {
-      this.SPHERE = sphere;
-      this.TO = to;
-      this.FROM = from;
-      this.TYPE = type;
-    }
+  // ...
 
-    public void execute(PylosGameIF game){
-      if(TYPE == ActionType.MOVE){
-        moveTo(game);
-      }
-      else if(TYPE == ActionType.REMOVE){
-        removeSphere(game);
-      }
-      else if(TYPE == ActionType.PASS){
-        pass(game);
-      }
-    }
-
-    private void moveTo(PylosGameIF game) {
-      game.moveSphere(SPHERE, TO);
-    }
-
-    private void removeSphere(PylosGameIF game) {
-      game.removeSphere(SPHERE);
-    }
-
-    private void pass(PylosGameIF game){
-      game.pass();
-    }
-
-    public void simulate(PylosGameSimulator sim){
-      if(TYPE == ActionType.MOVE){
-        sim.moveSphere(SPHERE, TO);
-      }
-      else if(TYPE == ActionType.REMOVE){
-        sim.removeSphere(SPHERE);
-      }
-      else if(TYPE == ActionType.PASS){
-        sim.pass();
-      }
-    }
-
+  public Action(PylosSphere sphere, PylosLocation to, PylosLocation from, ActionType type) {
+    this.SPHERE = sphere;
+    this.TO = to;
+    this.FROM = from;
+    this.TYPE = type;
   }
 
+  public void execute(PylosGameIF game) {
+    if (TYPE == ActionType.MOVE) {
+      game.moveSphere(SPHERE, TO);
+    } else if (TYPE == ActionType.REMOVE) {
+      game.removeSphere(SPHERE);
+    } else if (TYPE == ActionType.PASS) {
+      game.pass();
+    }
+  }
 
+  public void simulate(PylosGameSimulator sim) {
+    if (TYPE == ActionType.MOVE) {
+      sim.moveSphere(SPHERE, TO);
+    } else if (TYPE == ActionType.REMOVE) {
+      sim.removeSphere(SPHERE);
+    } else if (TYPE == ActionType.PASS) {
+      sim.pass();
+    }
+  }
 
+  public void undoSimulate(PylosGameSimulator sim) {
+    if (TYPE == ActionType.MOVE) {
+      if (prevState == PylosGameState.MOVE) {
+        sim.undoMoveSphere(this.SPHERE, this.FROM, this.prevState, this.SPHERE.PLAYER_COLOR.other());
+      } else if (this.FROM == null) {
+        sim.undoAddSphere(SPHERE, this.prevState, this.SPHERE.PLAYER_COLOR.other());
+      } else {
+        assert false;
+      }
+    } else if (TYPE == ActionType.REMOVE) {
+      if (prevState == PylosGameState.REMOVE_FIRST) {
+        sim.undoRemoveFirstSphere(SPHERE, FROM, prevState, this.SPHERE.PLAYER_COLOR.other());
+      } else if (prevState == PylosGameState.REMOVE_SECOND) {
+        sim.undoRemoveSecondSphere(SPHERE, FROM, prevState, this.SPHERE.PLAYER_COLOR.other());
+      } else {
+        assert false;
+      }
+    } else if (TYPE == ActionType.PASS) {
+      sim.undoPass(prevState, this.SPHERE.PLAYER_COLOR.other());
+    } else {
+      assert false;
+    }
+  }
 
+}
