36b49a00558c49b2a69393694f9cb37f6db5b935
Indra Coppens <115002315+Indra2003c@users.noreply.github.com>
2024-12-02 21:48:10 +0100

    back in comments to test further

--- ai_pylos/ai_pylos/ai_pylos-main/pylos-ml/src/main/java/be/kuleuven/pylos/PylosMLReinforcementTrainer.java
+++ ai_pylos/ai_pylos/ai_pylos-main/pylos-ml/src/main/java/be/kuleuven/pylos/PylosMLReinforcementTrainer.java
@@ -35,56 +35,56 @@ public class PylosMLReinforcementTrainer {
         //SavedModelBundle newModel;
         System.out.println("test 1");
         try (SavedModelBundle model = SavedModelBundle.load(MODEL_PATH, "serve")) {
-            try (SavedModelBundle model2 = SavedModelBundle.load(MODEL_PATH_2, "serve")) {
-                System.out.println("Model loaded");
+        //     try (SavedModelBundle model2 = SavedModelBundle.load(MODEL_PATH_2, "serve")) {
+        //         System.out.println("Model loaded");
 
-                PylosPlayerType trainedPlayer = new PylosPlayerType("ML") {
-                    @Override
-                    public PylosPlayer create() {
-                        return new PylosPlayerML(model);
-                    }
-                };
+        //         PylosPlayerType trainedPlayer = new PylosPlayerType("ML") {
+        //             @Override
+        //             public PylosPlayer create() {
+        //                 return new PylosPlayerML(model);
+        //             }
+        //         };
 
-                // tegen een oudere versie van ml spelen
-                PylosPlayerType trainedPlayer2 = new PylosPlayerType("ML2") {
-                    @Override
-                    public PylosPlayer create() {
-                        return new PylosPlayerML(model2);
-                    }
-                };
+        //         // tegen een oudere versie van ml spelen
+        //         PylosPlayerType trainedPlayer2 = new PylosPlayerType("ML2") {
+        //             @Override
+        //             public PylosPlayer create() {
+        //                 return new PylosPlayerML(model2);
+        //             }
+        //         };
 
-                BattleResult br = BattleMT.play(trainedPlayer, trainedPlayer2, 1000, 8, true); // 100000
-                playedGames.addAll(br.playedGames);
+        //         BattleResult br = BattleMT.play(trainedPlayer, trainedPlayer2, 1000, 8, true); // 100000
+        //         playedGames.addAll(br.playedGames);
 
                 
 
-                System.out.println("Played games: " + playedGames.size());
+        //         System.out.println("Played games: " + playedGames.size());
 
-                // Export to json file
-                File file = new File(EXPORT_PATH);
-                Files.createDirectories(file.getParentFile().toPath());
-                FileWriter writer = new FileWriter(file);
-                Gson gson = new GsonBuilder().create();
+        //         // Export to json file
+        //         File file = new File(EXPORT_PATH);
+        //         Files.createDirectories(file.getParentFile().toPath());
+        //         FileWriter writer = new FileWriter(file);
+        //         Gson gson = new GsonBuilder().create();
 
-                gson.toJson(playedGames, writer);
+        //         gson.toJson(playedGames, writer);
 
-                writer.flush();
-                writer.close();
+        //         writer.flush();
+        //         writer.close();
 
-                System.out.println("Exported to: reinforce_games");
+        //         System.out.println("Exported to: reinforce_games");
 
-                // https://scikit-learn.org/0.15/modules/scaling_strategies.html
-                // improve model with new data
-                // newModel = model.
+        //         // https://scikit-learn.org/0.15/modules/scaling_strategies.html
+        //         // improve model with new data
+        //         // newModel = model.
 
-                // model.fit(boards, scores, epochs=EPOCHS, batch_size=BATCH_SIZE);
+        //         // model.fit(boards, scores, epochs=EPOCHS, batch_size=BATCH_SIZE);
 
-                // model.save(MODEL_EXPORT_PATH + "reinforcement_test");
-                // model.export(MODEL_EXPORT_PATH + "latest");
+        //         // model.save(MODEL_EXPORT_PATH + "reinforcement_test");
+        //         // model.export(MODEL_EXPORT_PATH + "latest");
 
-            }
+        //     }
 
-        }
+         }
      }
 
 }
