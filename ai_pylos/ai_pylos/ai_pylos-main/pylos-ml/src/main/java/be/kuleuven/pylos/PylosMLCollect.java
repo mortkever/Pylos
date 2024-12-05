1914afbf54fc7b39dc9b80673175be77e6882c73
Victor <57635691+mortkever@users.noreply.github.com>
2024-12-05 09:14:00 +0100

    switch colors + player bit

--- ai_pylos/ai_pylos/ai_pylos-main/pylos-ml/src/main/java/be/kuleuven/pylos/PylosMLCollect.java
+++ ai_pylos/ai_pylos/ai_pylos-main/pylos-ml/src/main/java/be/kuleuven/pylos/PylosMLCollect.java
@@ -3,6 +3,7 @@ package be.kuleuven.pylos;
 import be.kuleuven.pylos.battle.BattleMT;
 import be.kuleuven.pylos.battle.BattleResult;
 import be.kuleuven.pylos.battle.data.PlayedGame;
+import be.kuleuven.pylos.game.PylosPlayerColor;
 import be.kuleuven.pylos.player.PylosPlayer;
 import be.kuleuven.pylos.player.PylosPlayerType;
 import be.kuleuven.pylos.player.codes.PylosPlayerBestFit;
@@ -23,13 +24,15 @@ import java.util.Date;
 
 public class PylosMLCollect {
     public static String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-    public static final String EXPORT_PATH = "pylos-ml/src/main/training/resources/games/"+timestamp+".json"; //0
-    //public static final String EXPORT_PATH = "pylos-ml/src/main/training/resources/games/test_set.json";
+    public static final String EXPORT_PATH = "pylos-ml/src/main/training/resources/games/" + timestamp + ".json"; // 0
+    // public static final String EXPORT_PATH =
+    // "pylos-ml/src/main/training/resources/games/test_set.json";
 
     public static void main(String[] args) throws IOException {
         // Collect games
         List<PlayedGame> playedGames = PylosMLCollect.collectGames();
-
+        playedGames = addMoveColor(playedGames, true);
+        playedGames.addAll(addMoveColor(switchPlayers(playedGames), false));
         System.out.println("Played games: " + playedGames.size());
 
         // Export to json file
@@ -47,7 +50,7 @@ public class PylosMLCollect {
     }
 
     public static List<PlayedGame> collectGames() {
-        ArrayList<PylosPlayerType> players =new ArrayList<>();
+        ArrayList<PylosPlayerType> players = new ArrayList<>();
         ArrayList<PlayedGame> playedGames = new ArrayList<>();
 
         PylosPlayerType p1 = new PylosPlayerType("BF") {
@@ -56,7 +59,7 @@ public class PylosMLCollect {
                 return new PylosPlayerBestFit();
             }
         };
-        players.add(0,p1);
+        players.add(0, p1);
 
         PylosPlayerType p2 = new PylosPlayerType("MM2") {
             @Override
@@ -64,7 +67,7 @@ public class PylosMLCollect {
                 return new PylosPlayerMiniMax(2);
             }
         };
-        players.add(1,p2);
+        players.add(1, p2);
 
         PylosPlayerType p3 = new PylosPlayerType("StudentPlayer_VictorIndra") {
             @Override
@@ -72,7 +75,7 @@ public class PylosMLCollect {
                 return new StudentPlayer_VictorIndra();
             }
         };
-        players.add(2,p3);
+        players.add(2, p3);
 
         PylosPlayerType p4 = new PylosPlayerType("MM4") {
             @Override
@@ -80,26 +83,131 @@ public class PylosMLCollect {
                 return new PylosPlayerMiniMax(5);
             }
         };
-        players.add(2,p4);
+        players.add(2, p4);
 
         // for(int i = 0; i<players.size();i++){
-        //     for(int j = 0; j<players.size();j++){
-        //         BattleResult br = BattleMT.play(players.get(i), players.get(j), 100000, 8, true); //100000
-        //         playedGames.addAll(br.playedGames);
-        //     }
-           
+        // for(int j = 0; j<players.size();j++){
+        // BattleResult br = BattleMT.play(players.get(i), players.get(j), 100000, 8,
+        // true); //100000
+        // playedGames.addAll(br.playedGames);
         // }
 
-        //test new resources
-            BattleResult br = BattleMT.play(p3, p4, 10, 8, true); //100000
-            playedGames.addAll(br.playedGames);
-        
-        
+        // }
 
-        
-        
+        // test new resources
+        BattleResult br = BattleMT.play(p3, p4, 10, 8, true); // 100000
+        playedGames.addAll(br.playedGames);
+
         return playedGames;
     }
+
+    private static List<PlayedGame> switchPlayers(List<PlayedGame> games) {
+        List<PlayedGame> switchedGames = new ArrayList<>();
+
+        for (PlayedGame game : games) {
+            PylosPlayerType lightPlayer = new PylosPlayerType(game.lightPlayer) {
+                public PylosPlayer create() {
+                    return new PylosPlayerMiniMax(5); // houvast geen eigenlijk nut
+                }
+            };
+            PylosPlayerType darkPlayer = new PylosPlayerType(game.darkPlayer) {
+                public PylosPlayer create() {
+                    return new PylosPlayerMiniMax(5); // houvast geen eigenlijk nut
+                }
+            };
+
+            PylosPlayerColor winner = switch (game.winner) {
+                case 1 -> PylosPlayerColor.DARK; // switched player colors winner
+                case -1 -> PylosPlayerColor.LIGHT;
+                case 0 -> null;
+                default -> null;
+            };
+
+            List<Long> boardHistory = new ArrayList<>();
+            for (Long board : game.boardHistory) {
+                Long newBoard = 0L;
+                for (int i = 0; i < 30; i++) {
+                    Long temp = board;
+                    temp = temp << 62 - 2 * i;
+                    temp = temp >>> 62;
+                    if (temp != 0L) {
+                        temp = temp == 1 ? 2L : 1L;
+                        temp = temp << 2 * i;
+                        newBoard = temp | newBoard;
+                    }
+
+                }
+                boardHistory.add(newBoard);
+            }
+
+            switchedGames.add(new PlayedGame(boardHistory, lightPlayer, darkPlayer, winner));
+
+        }
+
+        return switchedGames;
+    }
+
+    private static List<PlayedGame> mirrorPlayers(List<PlayedGame> games) {
+        List<PlayedGame> switchedGames = new ArrayList<>();
+
+        for (PlayedGame game : games) {
+            PylosPlayerType lightPlayer = new PylosPlayerType(game.lightPlayer) {
+                public PylosPlayer create() {
+                    return new PylosPlayerMiniMax(5); // houvast geen eigenlijk nut
+                }
+            };
+            PylosPlayerType darkPlayer = new PylosPlayerType(game.darkPlayer) {
+                public PylosPlayer create() {
+                    return new PylosPlayerMiniMax(5); // houvast geen eigenlijk nut
+                }
+            };
+
+            PylosPlayerColor winner = switch (game.winner) {
+                case 1 -> PylosPlayerColor.LIGHT;
+                case -1 -> PylosPlayerColor.DARK;
+                case 0 -> null;
+                default -> null;
+            };
+
+            List<Long> boardHistory = new ArrayList<>();
+            for (Long board : game.boardHistory) {
+                Long newBoard = 0L;
+                for (int i = 0; i < 30; i++) {
+                    Long temp = board;
+                    temp = temp << 62 - 2 * i;
+                    temp = temp >>> 62;
+                    if (temp != 0L) {
+                        temp = temp == 1 ? 2L : 1L;
+                        temp = temp << 2 * i;
+                        newBoard = temp | newBoard;
+                    }
+
+                }
+                boardHistory.add(newBoard);
+            }
+
+            switchedGames.add(new PlayedGame(boardHistory, lightPlayer, darkPlayer, winner));
+
+        }
+
+        return switchedGames;
+    }
+
+    public static List<PlayedGame> addMoveColor(List<PlayedGame> games, boolean lightFirst) {
+        for (PlayedGame game : games) {
+            for (int i = 0; i < game.boardHistory.size(); i++) {
+                game.boardHistory.set(i, game.boardHistory.get(i) + ((i % 2 == (lightFirst ? 0 : 1) ? 0L : 1L << 60)));
+                System.out.println(padleft(Long.toBinaryString(game.boardHistory.get(i))));
+            }
+        }
+        return games;
+    }
+
+    public static String padleft(String input) {
+        int lengte = input.length();
+        for (int i = 0; i < 64 - lengte; i++)
+            input = "0" + input;
+
+        return input;
+    }
 }
-
-
